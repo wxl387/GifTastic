c2c0fb3ae54f0334e48f217893f4fff57d839897
wxl387 <lixuyao.tony@gmail.com>
2017-08-17 00:38:23 -0400

    first draft
    rating shows up, but image does not display

---
+++ assets/javascript/app.js
@@ -0,0 +1,58 @@
+var animals=["cat", "dog", "rabbit"];
+
+function displayAnimal() {
+	var animal = $(this).attr("data-name");
+	var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=4b98921f9cb7415a89ed0b84a1236043&q=" + animal;
+	$.ajax ( {
+		url: queryURL,
+		method:"GET"
+	}).done(function(response) {
+		console.log(response);
+		var animalDiv = $("<div class='animal'>");
+		var rating;
+		var pOne;
+		var imgURL;
+		var image;
+
+		for (var i=0; i<response.data.length; i++) {
+			
+			rating = response.data[i].rating;
+			pOne = $("<p>").text("Rating: " + rating);
+			animalDiv.append(pOne);
+
+			imgURL = response.data[i].bitly_url;
+			image = $("<img>").attr("src", imgURL);
+			animalDiv.append(image);
+
+			$("#animalView").prepend(animalDiv);
+		}
+	});
+}
+
+function renderButtons() {
+	$("#btnView").empty();
+
+	for (var i=0; i<animals.length; i++) {
+		var a = $("<button>");
+
+		a.addClass("animal");
+		a.attr("data-name", animals[i]);
+		a.text(animals[i]);
+		$("#btnView").append(a);
+	}
+}
+
+$("#addAnimal").on("click", function(event) {
+	event.preventDefault();
+
+	var animal = $("animalInput").val().trim();
+
+	animals.push(animal);
+
+	renderButtons(); 
+})
+
+$(document).on("click", ".animal", displayAnimal);
+
+renderButtons();
+
